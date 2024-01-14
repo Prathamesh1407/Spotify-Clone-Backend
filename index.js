3f03c1991c024ed37e1da8386f4a1d1ce3385f49
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-01-14 12:29:11 +0530

    update

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ require("dotenv").config();
 const port=8080
 
 app.use(express.json())
-const corsOptions ={
-  origin:'http://localhost:3000', 
-  credentials:true,            
-  optionSuccessStatus:200,
+const corsOptions = (req, corsOptions) => {
+  const origin = req.headers.origin;
+  return {
+    origin,
+    credentials: true,
+    optionSuccessStatus: 200,
+  }
 }
 app.use(cors(corsOptions))
 
