26a633fe33fa7c9e2cb6510837f5352f42135c52
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-02-14 17:33:38 +0530

    update in cors

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-const express=require('express');
-const mongoose= require('mongoose');
-var JwtStrategy = require('passport-jwt').Strategy,
-ExtractJwt = require('passport-jwt').ExtractJwt;
-const passport=require("passport")
-const User=require("./models/User")
-const authRoutes=require("./routes/auth");
-const songRoutes = require('./routes/song');
-const playlistRoutes=require('./routes/playlist')
-const app=express();
-const cors=require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+var JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
+const passport = require("passport");
+const User = require("./models/User");
+const authRoutes = require("./routes/auth");
+const songRoutes = require("./routes/song");
+const playlistRoutes = require("./routes/playlist");
+const app = express();
+const cors = require("cors");
 require("dotenv").config();
-const port=8080
+const port = 8080;
 
-app.use(express.json())
-const corsOptions = (req, corsOptions) => {
-  const origin = req.headers.origin;
-  return {
-    origin,
-    credentials: true,
-    optionSuccessStatus: 200,
-  }
-}
-app.use(cors(corsOptions))
-app.options('*', cors(corsOptions));
+app.use(express.json());
+// const corsOptions = (req, corsOptions) => {
+//   const origin = req.headers.origin;
+//   return {
+//     origin,
+//     credentials: true,
+//     optionSuccessStatus: 200,
+//   }
+// }
+app.use(cors());
+// app.options('*', cors(corsOptions));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://spotify-clone-frontend-roan.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://spotify-clone-frontend-roan.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
 
@@ -37,28 +43,32 @@ app.use((req, res, next) => {
 });
 
 //connect mongoose to node app
-mongoose.connect("mongodb+srv://Prathamesh1407:"
-    +process.env.MONGO_PASSWORD+
-        "@cluster0.s78coer.mongodb.net/?retryWrites=true&w=majority"
-        ,
-        {
-            useNewurlParser:true,
-            useUnifiedTopology:true
-        }
-    ).then((x)=>{
-        console.log("Connected to Mongo")
-    }).catch((err)=>{
-        console.log("Error while Connecting to Mongo")
-    })
+mongoose
+  .connect(
+    "mongodb+srv://Prathamesh1407:" +
+      process.env.MONGO_PASSWORD +
+      "@cluster0.s78coer.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewurlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((x) => {
+    console.log("Connected to Mongo");
+  })
+  .catch((err) => {
+    console.log("Error while Connecting to Mongo");
+  });
 
 const opts = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'secret'
-  };
-  
-  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: "secret",
+};
+
+passport.use(
+  new JwtStrategy(opts, (jwt_payload, done) => {
     // Check if the user exists based on the payload information
-    User.findById(jwt_payload.identifier,(err, user) => {
+    User.findById(jwt_payload.identifier, (err, user) => {
       if (err) {
         return done(err, false);
       }
@@ -68,21 +78,16 @@ const opts = {
         return done(null, false);
       }
     });
-  }));
+  })
+);
 
+app.use("/auth", authRoutes);
+app.use("/song", songRoutes);
+app.use("/playlist", playlistRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
-
-
-
-
-app.use('/auth',authRoutes); 
-app.use('/song',songRoutes);
-app.use('/playlist',playlistRoutes)
-app.get('/',(req,res)=>{
-    res.send('Hello world')
-})
-
-
-app.listen(port,()=>{
-    console.log("App is running on port "+port)
-})
+app.listen(port, () => {
+  console.log("App is running on port " + port);
+});
